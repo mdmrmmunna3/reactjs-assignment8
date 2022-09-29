f4bcdc4cbe3bab811927b7a7ca3ee1d009079cab
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-09-29 23:30:41 +0600

    question added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Club from './components/Club/Club';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div >
       <Header></Header>
       <Club></Club>
+      <Question></Question>
     </div>
   );
 }
