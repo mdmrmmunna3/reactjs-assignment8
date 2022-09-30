d547e91293a594cf43724993bfd8e6d3c6112472
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-09-30 20:27:35 +0600

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,14 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header-container shadow'>
-            <div className='logo-titel'>
-                <img src={logo} alt="" />
-                <h2 className='header-text'>Health & Fitness Workout-Club</h2>
+            <div>
+                <div className='logo-titel'>
+                    <img src={logo} alt="" />
+                    <h2 className='header-text'>Health & Fitness Workout-Club</h2>
+                </div>
+                <p className='text-title'>Select today's Your Favorite exercise</p>
             </div>
+            
             <div className='link'>
                 <a href="/Home">Home</a>
                 <a href="/Contact-us">Blog</a>
